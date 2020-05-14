a7830193dc23ddd613e31772c48ea4f2e32aab3e
Jamdinho <contacto.jamdinho@gmail.com>
2020-05-14 23:48:54 +0200

    assets añadidos

---
+++ assets/js/go-to-the-top.js
@@ -0,0 +1,14 @@
+var btn = $('#totheTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 200) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
